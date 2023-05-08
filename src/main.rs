22745c39b7f711f5e343eee2255b5b7d3d143673
chyuhoin <694546200@qq.com>
2023-05-08 11:54:36 +0800

    输出lex.yy.c

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,23 @@ use crate::combine_dfa::{combine, print_standard_dfa};
 use crate::input::div_reg_action;
 use crate::minimize_dfa::minimize_dfa;
 use crate::input::init;
-use crate::output::print_partA;
+use crate::output::*;
 use crate::trans::translate_reg;
 use crate::to_dfa::{convert, print_dfa};
 
 fn main() {
     let s = init("../test.l");
-    //print_partA(&s[0]);
+    print_part_a(&s[0]);
     let longtxt = replace_reg(&s[1], &div_reg_action(&s[2]));
     let mut dfas = Vec::new();
-    for sent in longtxt {
-        print!("-------------\n[[{}]] [[{}]]\n--------------\n", sent.reg, sent.action);
+    for sent in &longtxt {
         if sent.reg.starts_with("\"") || sent.reg.starts_with(".") {continue;}
         let tmp_dfa = minimize_dfa(&convert(&translate_reg(&sent.reg)));
         dfas.push(tmp_dfa);
     }
     let final_dfa = combine(&dfas);
-    print_standard_dfa(&final_dfa);
+    print_part_b(&final_dfa);
+    print_part_c(&final_dfa);
+    print_part_d(&longtxt);
+    print_part_e(&s[3])
 }
--- src/output.rs
+++ src/output.rs
@@ -1,5 +1,6 @@
 use std::fs::{OpenOptions, File};
 use std::io::prelude::*;
+use crate::input::Sentence;
 use crate::to_dfa::Dfa;
 use crate::combine_dfa::Vertex;
 
@@ -11,7 +12,10 @@ fn get_output_file() -> File{
     return file;
 }
 
-pub fn print_partA(text: &str) {
+/*
+输出partA是头文件、宏定义和函数定义等信息，原封不动复制即可
+ */
+pub fn print_part_a(text: &str) {
     let mut file = get_output_file();
     let shader = format!(r#"
 #include <stdio.h>
@@ -26,19 +30,140 @@ int yyleng = 0;
 int yywarp();
 void yylex();
     "#, text);
-    if let Err(e) = writeln!(file, "{}", shader.trim()) {
+    if let Err(e) = writeln!(file, "{}\n", shader.trim()) {
         println!("cannot write: {}", e);
     }
 }
 
-pub fn print_partB(dfa :&Dfa<Vertex, char>) {
+/*
+输出partB是边信息，枚举dfa里面的所有边并打印到目标文件
+ */
+pub fn print_part_b(dfa :&Dfa<Vertex, char>) {
     let mut edge_string = String::from("{-1, -1, 0},\n");
+    let mut edge_num = 0;
     for u in &dfa.points {
         if let Some(e) = dfa.graph.get(&u) {
             for v in e {
-                let one_edge = format!("{{{}, {}, {:?}}},\n", u.id, v.0.id, v.1);
+                let one_edge = format!("    {{{}, {}, {:?}}},\n", u.id, v.0.id, v.1);
                 edge_string.push_str(&one_edge);
+                edge_num += 1;
             }
         }
+    }
+
+    let mut file = get_output_file();
+    let shader = format!(r#"
+typedef struct {{
+    int from;
+    int to;
+    char val;
+}} YYEdge;
+const YYEdge yy_edges[] = {{
+    {}
+}};
+const int yy_edge_num = {};
+    "#, edge_string.trim(), edge_num);
+    if let Err(e) = writeln!(file, "{}\n", shader.trim()) {
+        println!("cannot write: {}", e);
+    }
+}
+
+/*
+输出partC是点信息，枚举dfa里面的所有点的end信息并打印到目标文件
+ */
+pub fn print_part_c(dfa :&Dfa<Vertex, char>) {
+    let mut point_string = String::from("-1,\n");
+    let mut point_num = 0;
+    for u in &dfa.points {
+        let one_point = format!("    {},\n", u.end);
+        point_string.push_str(&one_point);
+        point_num += 1;
+    }
+
+    let mut file = get_output_file();
+    let shader = format!(r#"
+const int yy_vertexs_tag[] = {{
+    {}
+}};
+const int yy_vertex_num = {};
+
+int yy_dfa[{} << 1][200];
+void add_edge(int x, int y, char c) {{
+    yy_dfa[x][c] = y;
+}}
+
+    "#, point_string.trim(), point_num, point_num);
+    if let Err(e) = writeln!(file, "{}\n", shader.trim()) {
+        println!("cannot write: {}", e);
+    }
+}
+
+pub fn print_part_d(sents: &Vec<Sentence>) {
+    let mut action_string = String::from("");
+    for (i, sent) in sents.iter().enumerate() {
+        if sent.action == "." {
+            let one_action = format!("        default: {{{} break;}}\n", sent.action);
+        action_string.push_str(&one_action);
+        } else {
+            let one_action = format!("        case {}: {{{} break;}}\n", i, sent.action);
+            action_string.push_str(&one_action);
+        }
+    }
+
+    let mut file = get_output_file();
+    let shader = format!(r#"
+void yywork(int work) {{
+    switch (work) {{
+        {}
+    }}
+}}
+
+int yy_state;
+void yy_match(char c) {{
+    int forward = yy_dfa[yy_state][c];
+    if(forward == 0) {{
+        yywork(yy_vertexs_tag[yy_state]);
+        forward = yy_dfa[1][c];
+        yyleng = 0;
+    }}
+    yy_state = forward;
+}}
+    "#, action_string.trim());
+    if let Err(e) = writeln!(file, "{}\n", shader.trim()) {
+        println!("cannot write: {}", e);
+    }
+}
+
+pub fn print_part_e(text: &str) {
+    let mut file = get_output_file();
+    let shader = format!(r#"
+void yyinit() {{
+    if (yyin == NULL) yyin = stdin;
+    if (yyout == NULL) yyout = stdout;
+
+    yytext = malloc(sizeof(char) * 100);
+
+    int i;
+    for(i = 1; i <= yy_edge_num; i++) {{
+        add_edge(yy_edges[i].from, yy_edges[i].to, yy_edges[i].val);
+    }}
+    yy_state = 1;
+}}
+
+void yylex() {{
+    yyinit();
+
+    while(1) {{
+        char c = fgetc(yyin);
+        if (feof(yyin))  break;
+        yy_match(c);
+        yytext[yyleng++] = c;
+    }}
+
+}}
+{}
+    "#, text);
+    if let Err(e) = writeln!(file, "{}\n", shader.trim()) {
+        println!("cannot write: {}", e);
     }
 }
